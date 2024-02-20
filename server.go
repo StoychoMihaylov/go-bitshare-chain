7452219bef0e267cd2e95bfb1551b08c7205bb23
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2024-02-21 00:34:44 +0100

    add GIN and GORILLA to run on the same server

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	ginRouter := gin.Default()
+	ginRouter.GET("/gin", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello from GIN!")
+	})
+
+	gorillaRouter := mux.NewRouter()
+	gorillaRouter.HandleFunc("/gorilla", func(response http.ResponseWriter, request *http.Request) {
+		response.Write([]byte("Hello from GORILLA Mux!"))
+	}).Methods("GET")
+
+	// Create a single server for GIN and GORILLA
+	http.Handle("/gin", ginRouter)
+	http.Handle("/gorilla", gorillaRouter)
+
+	http.ListenAndServe(":8080", nil)
+}
