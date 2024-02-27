78038102d7ce596976e1dc6c004f3b1dd0d9970a
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2024-02-27 20:43:55 +0100

    all blockchain class components ready

--- Infrastructure/services/key-generator.go
+++ Infrastructure/services/key-generator.go
@@ -1,7 +1,30 @@
 package services
 
-type KeyGeneratorService interface {
+import (
+	ecdsa "crypto/ecdsa"
+	elliptic "crypto/elliptic"
+	rand "crypto/rand"
+	hex "encoding/hex"
+	fmt "fmt"
+)
+
+type IKeyGenerator interface {
 	GeneratePublicAndPrivateKey() (publicKey string, privateKey string)
 }
 
-type service struct{}
+type KeyGenerator struct{}
+
+func (kg *KeyGenerator) GeneratePublicAndPrivateKey() (publicKey string, privateKey string) {
+	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		panic(fmt.Errorf("failed to generate key: %v", err))
+	}
+
+	publicKeyBytes := elliptic.Marshal(key.PublicKey.Curve, key.PublicKey.X, key.PublicKey.Y)
+	privateKeyBytes := key.D.Bytes()
+
+	publicKey = hex.EncodeToString(publicKeyBytes)
+	privateKey = hex.EncodeToString(privateKeyBytes)
+
+	return publicKey, privateKey
+}
